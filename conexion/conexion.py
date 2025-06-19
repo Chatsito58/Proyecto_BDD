cb6b8600aea1b9b64ae614525c0ee220b22026e3
Chatsito GPT <chatsitogpt58@gmail.com>
2025-06-19 14:20:13 -0500

    Fix sync retries for empleado table

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -103,34 +103,20 @@ class ConexionBD:
                 else:
                     self.conn.commit()
                 cur.close()
-            except Error as c:
-                logging.error('Error al sincronizar: %s', c)
-                if (
-                    "clientes" in op['query'].lower()
-                    and "doesn't exist" in str(c)
-                ):
-                    corregida = op['query'].replace('clientes', 'cliente')
-                    logging.info('Reintentando con tabla: %s', corregida)
-                    try:
-                        cur.execute(corregida, op['params'])
-                        if corregida.strip().lower().startswith('select'):
-                            cur.fetchall()
-                        else:
-                            self.conn.commit()
-                        cur.close()
-                        continue
-                    except Error as c2:  # pragma: no cover - depende de MySQL
-                        logging.error('Error tras corregir: %s', c2)
-                        op['query'] = corregida
-                self.pendientes.insert(0, op)
-                break
             except Error as e:
                 logging.error('Error al sincronizar: %s', e)
+                corregida = None
                 if (
+                    "clientes" in op['query'].lower()
+                    and "doesn't exist" in str(e)
+                ):
+                    corregida = op['query'].replace('clientes', 'cliente')
+                elif (
                     "empleados" in op['query'].lower()
                     and "doesn't exist" in str(e)
                 ):
                     corregida = op['query'].replace('empleados', 'empleado')
+                if corregida:
                     logging.info('Reintentando con tabla: %s', corregida)
                     try:
                         cur.execute(corregida, op['params'])
